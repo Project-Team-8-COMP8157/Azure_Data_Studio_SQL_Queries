e7b055b5cff45ee1677f31baf78dab569f85643f
DasSwagato <91630024+DasSwagato@users.noreply.github.com>
2022-03-28 15:22:45 -0400

    Update azure_queries.sql

--- azure_queries.sql
+++ azure_queries.sql
@@ -2,7 +2,5 @@ SELECT TOP (1000) * FROM [master].[dbo].[ca-500_address];
 SELECT COUNT(*) FROM [master].[dbo].[ca-500_address];
 
 INSERT INTO [master].[dbo].[ca-500_customer] VALUES ('0','FName', 'LName', 'CName', '9876541111', '9876541112', 'email', 'web');
-
 SELECT * FROM [master].[dbo].[ca-500_customer] WHERE S_No='1';
-
 DELETE FROM [master].[dbo].[ca-500_customer] WHERE S_No='1';
