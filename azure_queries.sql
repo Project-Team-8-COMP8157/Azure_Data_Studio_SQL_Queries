3c662c1de2d4983712361f04da311d930395f3df
VenkataNaveenKumarAkula <91692154+VenkataNaveenKumarAkula@users.noreply.github.com>
2022-03-31 18:07:35 -0400

    Update azure_queries.sql

--- azure_queries.sql
+++ azure_queries.sql
@@ -1,5 +1,4 @@
 SELECT TOP (1000) * FROM [master].[dbo].[ca-500_address];
-
 SELECT COUNT(*) FROM [master].[dbo].[ca-500_address];
 
 INSERT INTO [master].[dbo].[ca-500_customer] VALUES ('0','FName', 'LName', 'CName', '9876541111', '9876541112', 'email', 'web');
